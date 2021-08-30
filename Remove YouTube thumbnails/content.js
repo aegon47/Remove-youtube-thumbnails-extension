fcc4fb83fd26639909024cacd824a87bf9d08524
aegon47 <chandu199827@gmail.com>
2021-08-30 20:16:41 +0530

    Initial commit
    Added code and files

---
+++ Remove YouTube thumbnails/content.js
@@ -0,0 +1,7 @@
+var hehe = document.getElementsByClassName("style-scope yt-img-shadow");
+for (var i = 0; i < hehe.length; ++i) {
+    var item = hehe[i];  
+    item.style.filter = 'blur(100px)';
+  //item.style.display = 'none';
+}
+
